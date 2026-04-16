2e916bf43c053da5c86bcb9774f05b7ce75bb5b1
Geethika B <geethika3338@gmail.com>
2026-04-16 20:22:03 +0530

    implemented productservice

---
+++ src/main/java/com/pawpalace/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.pawpalace.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pawpalace.model.Product;
+import com.pawpalace.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product addProduct(Product product) {
+        return repository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        repository.deleteById(id);
+    }
+}
